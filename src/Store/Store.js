688480faa95d939ed47c5bf69180916931337bf8
JakubSys <32591413+JakubSys@users.noreply.github.com>
2018-08-23 13:27:24 +0200

    Redux created

---
+++ src/Store/Store.js
@@ -0,0 +1,23 @@
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import { routerMiddleware } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import reducers from '../Reducers';
+
+const enhancers = [];
+export const history = createHistory();
+const middleware = routerMiddleware(history);
+
+if (process.env.NODE_ENV === 'development') {
+    const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
+    if (typeof devToolsExtension === 'function') {
+        enhancers.push(devToolsExtension());
+    }
+}
+const store = createStore(
+    reducers,
+    {},
+    compose(applyMiddleware(thunk, middleware), ...enhancers)
+);
+export default store;
